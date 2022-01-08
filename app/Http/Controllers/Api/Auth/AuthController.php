16db026561a247fb3a0bc198ed600bab3d05893e
oluwablin <you@oyelekeseun.com>
2022-01-08 09:38:46 +0100

    laravel passport installed

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Create new User.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function createUser()
+    {
+        //
+    }
+
+    /**
+     * Login a User.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function loginUser(Request $request)
+    {
+        //
+    }
+
+}
